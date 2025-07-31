bfed13551f20445555d4fb90a532b4a8f76ee4a2
Marlon Silva <marlon.m_silva@outlook.com>
2025-07-31 14:50:41 -0300

    Finalized API

--- src/main/java/marlonsilvadev/app/com/ws_products/controller/produtoController.java
+++ src/main/java/marlonsilvadev/app/com/ws_products/controller/produtoController.java
@@ -1,5 +1,6 @@
 package marlonsilvadev.app.com.ws_products.controller;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import marlonsilvadev.app.com.ws_products.model.Product;
 import marlonsilvadev.app.com.ws_products.repository.ProductRepository;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/products")
@@ -23,19 +29,70 @@ public class ProdutoController {
     }
 
     @PostMapping
-    public Product save(@RequestBody Product product) {
-        System.out.println("Product saved: " + product);
-        var id = UUID.randomUUID().toString();
-        product.setId(id);
+    public ResponseEntity<Void> save(@RequestBody Product product) {
+        try {
+            System.out.println("Product saved: " + product);
+            var id = UUID.randomUUID().toString();
+            product.setId(id);
 
-        productRepository.save(product);
-        return product;
+            productRepository.save(product);
+            return ResponseEntity.ok().build();
+        } catch (NumberFormatException e) {
+            System.out.println("Error parsing product ID: " + e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
     }
 
-    @GetMapping("/{id}")
-    public Product get(@PathVariable("id") String id) {
-        System.out.println("Product ID requested: " + id);
-        return productRepository.findById(id).orElse(null);
+    @GetMapping("{id}")
+    public ResponseEntity<Product> get(@PathVariable("id") String id) {
+        try {
+            System.out.println("Product ID requested: " + id);
+            return ResponseEntity.ok(productRepository.findById(id).orElse(null));
+        } catch (NumberFormatException e) {
+            System.out.println("Error parsing product ID: " + e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") String id) {
+        try {
+
+            if (!productRepository.existsById(id)) {
+                return ResponseEntity.notFound().build();
+            }
+
+            productRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> update(@PathVariable("id") String id, @RequestBody Product product) {
+        try {
+            if (!productRepository.existsById(id)) {
+                return ResponseEntity.notFound().build();
+            }
+            product.setId(id);
+            System.out.println("Product updated: " + product);
+            productRepository.save(product);
+
+            return ResponseEntity.ok().build();
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getList(@RequestParam("name") String name) {
+        try {
+            return ResponseEntity.ok(productRepository.findByName(name));
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
 }
--- src/main/java/marlonsilvadev/app/com/ws_products/repository/ProductRepository.java
+++ src/main/java/marlonsilvadev/app/com/ws_products/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package marlonsilvadev.app.com.ws_products.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import marlonsilvadev.app.com.ws_products.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
 
+    List<Product> findByName(String name);
 }
